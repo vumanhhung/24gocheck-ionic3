7cdcb01d74d33ef12b181b23c2bb7966571ac5b8
Shikamaru <hero4120@gmail.com>
2018-03-26 10:22:41 +0700

    fix user wallet

--- src/pages/accounts/wallet/delivery-point/delivery-point.ts
+++ src/pages/accounts/wallet/delivery-point/delivery-point.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AccountsProvider } from '../../../../providers/accounts/accounts';
 
 /**
  * Generated class for the DeliveryPointPage page.
@@ -15,11 +16,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DeliveryPointPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  receiverAccount: string;
+  receiverId: number;
+  receiverName: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public accountService: AccountsProvider) {
+    this.receiverAccount = '';
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DeliveryPointPage');
+  }
+
+  checkAccountExist(ev: any) {
+
+    let val = ev.target.value;
+
+    if(val === '' || val === null) {
+      this.receiverId = null;
+      this.receiverName = '';
+    } else {
+      this.accountService.getReceiverInfo(val).subscribe(data => {
+        this.receiverId = data['receiver_id'];
+
+        if(this.receiverId) {
+          this.receiverName = `${data['lastname']} ${data['firstname']}`;          
+        } else {
+          this.receiverName = `Tài khoản hưởng không tồn tại`;
+        }
+
+
+        console.log(`Receiver_id is ${this.receiverId} and Name is ${this.receiverName}`);
+      });
+    }
   }
 
 }
--- src/pages/accounts/wallet/wallet.ts
+++ src/pages/accounts/wallet/wallet.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { AddPointPage } from './add-point/add-point';
 import { DeliveryPointPage } from './delivery-point/delivery-point';
 import { HistoryTransactionPage } from './history-transaction/history-transaction';
+import { AccountsProvider } from '../../../providers/accounts/accounts';
 
 /**
  * Generated class for the WalletPage page.
@@ -19,11 +20,15 @@ export class WalletPage {
   addPointPage: any;
   deliveryPointPage: any;
   historyTransactionPage: any;
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  customer_point: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public accountService: AccountsProvider) {
     this.addPointPage = AddPointPage;
     this.deliveryPointPage = DeliveryPointPage;
     this.historyTransactionPage = HistoryTransactionPage;
+
+    this.customer_point = accountService.getUserInfo()['point'];
+
+    // console.log('U info',accountService.getUserInfo());
   }
 
   ionViewDidLoad() {
